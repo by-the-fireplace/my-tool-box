7341c2ebf09b83c50e848c3a3e25fcbde3268b8f
jingquan-wang <jq.wang1214@gmail.com>
2022-01-08 01:22:34 -0800

    add test

---
+++ test/test_homemade_knn.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from my_toolbox.ml.knn import HomemadeKNNClassifier
+
+
+def test_homemade_knn():
+    x_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [2, 0]])
+    y_train = np.array(["a", "a", "a", "b", "c"])
+    x_test = np.array([[0.5, 0]])
+    knn_classifier = HomemadeKNNClassifier(k=3)
+    knn_classifier.fit(x_train, y_train)
+    y_pred = knn_classifier.predict(x_test)
+    assert y_pred.tolist() == ["a"]
