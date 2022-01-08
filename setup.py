df3fffa496e46f6f2da02e84b5c27546030f44d3
jingquan-wang <jq.wang1214@gmail.com>
2022-01-08 01:21:32 -0800

    package init

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="my_toolbox",
+    version="1.0",
+    description="My toolbox for everything",
+    author="Jing-Quan Wang",
+    author_email="jq.wang1214@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
